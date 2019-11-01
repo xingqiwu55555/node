8699db863c9930ceb8dac0ecee2e50febdc6d208
shenxiaomeng <xmshen@thoughtworks.com>
2019-11-01 14:05:58 +0800

    [feat | geek_node]: node nonblocking

---
+++ geek_node/nonblocking/index.js
@@ -0,0 +1,20 @@
+const glob = require('glob');
+
+// 阻塞 I/O
+// var result = null;
+// console.time('glob');
+// result = glob.sync(__dirname + '/**/*');
+// console.timeEnd('glob');
+// console.log(result);
+
+
+// 非阻塞 I/O
+var result = null;
+console.time('glob');
+glob(__dirname + '/**/*', function(err, res) {
+  result = res;
+  // console.log(result);
+  console.log('got glob');
+});
+console.timeEnd('glob');
+console.log(1 + 1);
