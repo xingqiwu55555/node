53610ab252dcc709473bec1f21dc701eb2e69b33
shenxiaomeng <xmshen@thoughtworks.com>
2019-11-05 14:21:38 +0800

    [feat | geek_node]: rpc buffer

--- geek_node/rpc-buffer/index.js
+++ geek_node/rpc-buffer/index.js
@@ -1,2 +1,31 @@
 const buffer1 = Buffer.from('geekbang');
 const buffer2 = Buffer.from([1, 2, 3, 4]);
+
+const buffer3 = Buffer.alloc(20);
+
+console.log(buffer1);
+console.log(buffer2);
+console.log(buffer3);
+
+buffer2.writeInt8(12, 1);
+console.log(buffer2);
+buffer2.writeInt16BE(512, 2);
+console.log(buffer2);
+buffer2.writeInt16LE(512, 2);
+console.log(buffer2);
+
+const fs = require('fs');
+const protobuf = require('protocol-buffers');
+const schema = protobuf(fs.readFileSync(__dirname + '/test.proto', 'utf-8'));
+
+console.log('schema', schema);
+
+const buffer = schema.Course.encode({
+  id: 1,
+  name: 'Node.js',
+  price: 80.4
+});
+
+console.log(buffer);
+console.log(schema.Course.decode(buffer));
+
